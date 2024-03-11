8dad3d0c355784a2d56f83eed43d70687092a264
Gaillard05 <maylisgaillard@hotmail.fr>
2024-03-11 09:40:47 +0100

    verrification longeur des donnees et format du numero de telephone

--- src/verifRequest.middleware.ts
+++ src/verifRequest.middleware.ts
@@ -21,6 +21,32 @@ export class RequestValidationMiddleware implements NestMiddleware {
       });
     }
 
+    // Vérifier si les champs requis ne sont pas vides
+    if (name.trim() === '' || telephone.trim() === '') {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        error: 'Requête invalide',
+        message: 'Le nom et le numéro de téléphone ne peuvent pas être vides',
+      });
+    }
+
+     // Vérifier la longueur du nom (par exemple, supposons que le nom doit avoir au moins 3 caractères)
+     if (name.length < 3) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        error: 'Requête invalide',
+        message: 'Le nom doit comporter au moins 3 caractères',
+      });
+    }
+
+
+     // Vérifier la longueur du numéro de téléphone
+     const phoneNumberRegex = /^\d{2}\.\d{2}\.\d{2}\.\d{2}\.\d{2}$/; // Format xx.xx.xx.xx.xx
+     if (!phoneNumberRegex.test(telephone)) {
+       return res.status(HttpStatus.BAD_REQUEST).json({
+         error: 'Requête invalide',
+         message: 'Le numéro de téléphone doit être au format xx.xx.xx.xx.xx',
+       });
+     }
+
     // Si toutes les vérifications passent, passer au middleware suivant
     next();
   }
